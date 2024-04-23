aa91e874236a958803d3b4cddc747df9e8ce09a1
Avazbek <avazbek@gmail.com>
2024-04-24 00:35:22 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Talaba talaba = new Talaba();
+        Console.WriteLine("Ism");
+        talaba.Name = Console.ReadLine();
+        Console.WriteLine("Familiya");
+        talaba.SurName = Console.ReadLine();
+        Console.WriteLine("O`qish joyi (OTM)");
+        talaba.Edu = Console.ReadLine();
+        Console.WriteLine("Kursi");
+        talaba.Id = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\n\t\t Talaba xaqida ma`lumot \n");
+        Console.WriteLine($"Talaba: {talaba.Name} {talaba.SurName}");
+        Console.WriteLine($"{talaba.Edu} bakalavr {talaba.Id} - bosqich talabasi");
+    }
+}
+
+class Talaba
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string SurName { get; set;}
+    public string Edu { get; set;}
+}
